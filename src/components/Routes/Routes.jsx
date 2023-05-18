72c166a32ff5d17156ecee3426558e351c06d6be
developerMasum <developer.masum2001@gmail.com>
2023-05-19 01:13:30 +0600

    added env file

---
+++ src/components/Routes/Routes.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home";
+import AllToy from "../Pages/AllToy/AllToy";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+import PrivateRoute from "./PrivateRoute";
+import ToyDetails from "../Pages/Home/ToyDetails";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Main />
+  ,
+  children:[
+    {
+      path:'/',
+      element:<Home />
+    },
+    {
+      path:'/toys',
+      element:<AllToy />
+    },{
+      path:'/login',
+      element:<Login />
+    },
+    {
+      path:'/register',
+      element: <Register />
+    },
+    {
+      path:'/toys/:id',
+      element: <PrivateRoute> <ToyDetails/> </PrivateRoute>,
+      loader:({params})=>fetch(`http://localhost:5000/toys/${params.id}`)
+     
+    }
+  ]
+  },
+]);
+export default router;
